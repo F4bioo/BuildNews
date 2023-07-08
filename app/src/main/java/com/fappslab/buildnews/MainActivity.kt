9653dc79077b475f07d84a60a2977d7247553d4b
F4bioo <costa.fbo@gmail.com>
2023-07-08 15:03:07 -0300

    Added: [BUNE/2-3] Create buildSrc and gradle plugins [android-build.gradle, flavor-build.gradle, test-build.gradle]

--- app/src/main/java/com/fappslab/buildnews/MainActivity.kt
+++ app/src/main/java/com/fappslab/buildnews/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.fappslab.buildnews
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.fappslab.bookshelf.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.text_hello).apply {
+            text = "Hello from flavor: $packageName"
+        }
     }
 }
---
+++ buildSrc/src/main/java/Config.kt
@@ -0,0 +1,10 @@
+object Config {
+    private const val packageName = "com.fappslab.bookshelf"
+    const val namespace = packageName
+    const val compileSdk = 33
+    const val applicationId = packageName
+    const val minSdk = 28
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
